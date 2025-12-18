7bc5aaaa0f7efe06aceac8676dc9f4576e24a32c
CodeFFun <sharmasanketa899@gmail.com>
2025-12-18 17:33:51 +0545

    Input form theme data

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 
-ThemeData getApplicationTheme(){
+ThemeData getApplicationTheme() {
   return ThemeData(
+    inputDecorationTheme: InputDecorationTheme(
+      floatingLabelBehavior: FloatingLabelBehavior.never,
+      filled: true,
+      fillColor: Colors.grey.shade300,
+      labelStyle: TextStyle(color: Colors.grey.shade400),
+      hintStyle: TextStyle(color: Colors.grey.shade400),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        borderSide: BorderSide(color: Colors.grey),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        borderSide: BorderSide(color: Colors.red),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        borderSide: BorderSide(color: Colors.red),
+      ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        textStyle: const TextStyle(
-          fontSize: 20,
-          color: Colors.white
-        ),
+        textStyle: const TextStyle(fontSize: 20, color: Colors.white),
         backgroundColor: Color.fromARGB(255, 56, 36, 13),
         foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10)
-        ),
-      )
-    )
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    ),
   );
 }
