fc5fcb1aed220652fd4fbfae401a4f707185c171
CodeFFun <sharmasanketa899@gmail.com>
2025-12-18 17:18:01 +0545

    Elevated button theme data

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme(){
   return ThemeData(
-    
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+          fontSize: 20,
+          color: Colors.white
+        ),
+        backgroundColor: Color.fromARGB(255, 56, 36, 13),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)
+        ),
+      )
+    )
   );
 }
