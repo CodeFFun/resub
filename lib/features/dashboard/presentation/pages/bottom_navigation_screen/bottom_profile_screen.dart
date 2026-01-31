b3b7aa4b7001c0123c5f3f4f10581a9140e0c4d7
CodeFFun <sharmasanketa899@gmail.com>
2026-01-31 13:21:27 +0545

    Profile change screen

--- lib/features/dashboard/presentation/pages/bottom_navigation_screen/bottom_profile_screen.dart
+++ lib/features/dashboard/presentation/pages/bottom_navigation_screen/bottom_profile_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:resub/app/routes/app_routes.dart';
+import 'package:resub/core/services/storage/token_service.dart';
 import 'package:resub/core/services/storage/user_session_service.dart';
 import 'package:resub/features/auth/presentation/pages/login_screen.dart';
+import 'package:resub/features/profile/presentation/pages/personal_info_page.dart';
 
 class BottomProfileScreen extends ConsumerWidget {
   const BottomProfileScreen({super.key});
@@ -19,11 +21,31 @@ class BottomProfileScreen extends ConsumerWidget {
             padding: const EdgeInsets.all(16.0),
             child: SizedBox(
               width: double.infinity,
+              child: OutlinedButton(
+                onPressed: () {
+                  AppRoutes.push(context, const PersonalInfoPage());
+                },
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(color: Colors.brown),
+                ),
+                child: const Text(
+                  "Profile",
+                  style: TextStyle(color: Colors.brown),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: double.infinity,
               child: ElevatedButton(
                 onPressed: () async {
                   // Clear the userSessionServiceProvider
                   final userSession = ref.read(userSessionServiceProvider);
+                  final tokenSession = ref.read(tokenServiceProvider);
                   await userSession.clearSession();
+                  await tokenSession.removeToken();
 
                   ref.invalidate(userSessionServiceProvider);
 
